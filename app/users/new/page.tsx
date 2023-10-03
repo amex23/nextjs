c82d4b33724d5a0d939b9025438ad5f7936ced78
amex23 <randelb2022@gmail.com>
2023-10-04 00:05:01 +0800

    new commit

---
+++ app/users/new/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewUserPage = () => {
+  return (
+	<main>
+		<div>
+			UsersPage
+		</div>
+	</main>
+  )
+}
+
+export default NewUserPage
---
+++ app/users/page.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+interface User {
+	id: number;
+	name: string;
+	email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch(
+	'https://jsonplaceholder.typicode.com/users', 
+	{ cache: 'no-store' }); 
+  const users: User[] = await res.json();
+
+  const { Client } = require('@notionhq/client');
+
+	const notion = new Client({ auth: process.env.NOTION_API_KEY });
+	
+	(async () => {
+	  const pageId = 'Bible-Verse-fe3ee4c49a3e47869d489a3450077fd7';
+	  const response = await notion.pages.retrieve({ page_id: pageId });
+	  console.log(response);
+	})();
+
+  return (
+	<main className=''>
+		<h1>
+			Users
+		</h1>
+		{/* <p>{new Date().toLocaleTimeString()}</p> */}
+		<table className="table table-bordered">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+				</tr>
+			</thead>
+			<tbody>
+			{users.map(user => 
+			<tr key={user.id}>
+				<td>{user.name}</td>
+				<td>{user.email}</td>
+			</tr> )}
+			</tbody>
+			
+		</table>
+	</main>
+  )
+}
+export default UsersPage
